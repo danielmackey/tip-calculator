46391388401cd8852bdd774bc1c9d4c6f1bdb7e4
Daniel Mackey <dmackey@bellycard.com>
2014-07-18 12:51:59 -0500

    More UI behavior linked up to controller

--- tip-calculator/ViewController.swift
+++ tip-calculator/ViewController.swift
@@ -14,10 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet var taxPercentSlider: UISlider
     @IBOutlet var taxPercentLabel:  UILabel
     @IBOutlet var resultsTextView:  UITextView
+    
+    let tipCalculator = TipCalculatorModel(total: 33.25, taxPercent: 0.06)
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        refreshUI()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,16 +28,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func refreshUI() {
+        totalTextField.text = String(tipCalculator.total)
+        taxPercentSlider.value = Float(tipCalculator.taxPercent) * 100.0
+        taxPercentLabel.text = "Tax Percentage (\(Int(taxPercentSlider.value)))%:"
+        resultsTextView.text = ""
+    }
+    
     @IBAction func calculateTapped(sender: AnyObject) {
+        tipCalculator.total = Double(totalTextField.text.bridgeToObjectiveC().doubleValue)
         
+        let possibleTips = tipCalculator.returnPossibleTips()
+        var results = ""
+        
+        for(tipPrecent, tipValue) in possibleTips {
+            let prettyTipValue = String(format: "%.2f", tipValue)
+            results += "\(tipPrecent)%: $\(prettyTipValue)\n"
+        }
+        
+        resultsTextView.text = results
     }
     
     @IBAction func taxPercentageChanged(sender: AnyObject) {
-        
+        tipCalculator.taxPercent = Double(taxPercentSlider.value) / 100.0
+        refreshUI()
     }
     
     @IBAction func viewTapped(sender: AnyObject) {
-        
+        totalTextField.resignFirstResponder()
     }
 
 }
